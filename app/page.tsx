acac18933816160376fff02d7346c10eb4e6869f
Taha Saif <tahasaifsurhio454@gmail.com>
2024-10-24 11:40:11 +0500

    class-7 code of thursday 9 to 12 of GIAIC

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className='pl-[200px] my-[10px] font'>Hello World</h1>
+    <div className=" bg-secondary h-[400px] bg-no-repeat w-full bg-cover">
+      <h1 className=' my-[10px] bg-taha text-center font-bold text-[36px]'>Hello World</h1> 
+      {/* h1:pl-[10px]      */}
+      {/* <h1 className='my-[10%] bg-[#2eb6e8] text-center font-bold text-[36px] p-1'>Hello World</h1> */}
+      {/* bg-[#2eb6e8]  */}
+      {/* bg-[url('/Next-js.png')]  */}
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary:"#f6bf38",
+        secondary:"#ff6600",
+        taha:"green",
       },
     },
   },
